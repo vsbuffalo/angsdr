c0ed83a62cd911f0cafb0ed7de92fda2d5d9dbe1
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2014-03-06 01:00:33 -0800

    refactored out my old curmudgeon R in favor of Hadley's elegant solutions

--- R/load.R
+++ R/load.R
@@ -40,45 +40,36 @@
 #' @export
 readPestPG <- function(file, asGRanges=TRUE, physical=FALSE) {
 
-	cols <- c(region="character", chrom="factor", window.center="integer",
-						theta_w="numeric", theta_pi="numeric", theta_fuli="numeric", theta_fayh="numeric",
-						theta_l="numeric", taj_d="numeric", fuli_f="numeric", fuli_d="numeric",
-						fay_h="numeric", zeng_e="numeric", numsites="integer")
+  cols <- c(region="character", chrom="factor", window.center="integer",
+            theta_w="numeric", theta_pi="numeric", theta_fuli="numeric", theta_fayh="numeric",
+            theta_l="numeric", taj_d="numeric", fuli_f="numeric", fuli_d="numeric",
+            fay_h="numeric", zeng_e="numeric", numsites="integer")
 
-	if (physical && !asGRanges)
-		stop("physical=TRUE only applies to GRanges output (set asGRanges=TRUE)")
+  angsd_raw <- read.delim(file, header=FALSE, col.names=names(cols),
+													colClasses=cols, sep="\t")
 
-	tmp <- read.delim(file, header=FALSE, col.names=names(cols),
-										colClasses=cols, sep="\t")
+	# extract regions
+  region_str <- gsub("\\((\\d+),(\\d+)\\)\\((\\d+),(\\d+)\\)\\((\\d+),(\\d+)\\)",
+                     "\\1;;;\\2;;;\\3;;;\\4;;;\\5;;;\\6", angsd_raw$region, perl=TRUE)
 
-	region_str <- gsub("\\((\\d+),(\\d+)\\)\\((\\d+),(\\d+)\\)\\((\\d+),(\\d+)\\)",
-										 "\\1;;;\\2;;;\\3;;;\\4;;;\\5;;;\\6", tmp$region, perl=TRUE)
+	regcols <- c("index_start", "index_stop", "pos_start", "pos_stop", "reg_start", "reg_stop")
+  region_chunks <- setNames(ldply(strsplit(region_str, ";;;"), .fun=as.numeric), regcols)
 
-	region_chunks <- do.call(rbind,
-													 lapply(region_str,
-																	function(x) as.integer(unlist(strsplit(x, ";;;", fixed=TRUE)))))
-	# region start
-	tmp$start <- region_chunks[, 5]
-	tmp$end <- region_chunks[, 6]
+	# add parsed regions back in
+	tmp <- cbind(angsd_raw, region_chunks)
 
-	# included positions
-	tmp$inc_start <- region_chunks[, 3]
-	tmp$inc_end <- region_chunks[, 4]
+  if (physical) {
+    # use the physical range used
+		rcols <- c("reg_start", "reg_stop")
+  } else {
+		rcols <- c("pos_start", "pos_stop")
+  }
 
-	if (physical) {
-		# use the physical range used
-		start <- tmp$inc_start
-		end <- tmp$inc_end
-	} else {
-		start <- tmp$start
-		end <- tmp$end
-	}
-
-	if (asGRanges) {
-		out <- GRanges(tmp$chrom, IRanges(start, end))
-		elementMetadata(out) <- tmp[, names(cols)[4:14]]
-	} else {
-		out <- tmp[, c("chrom", "start", "end", names(cols)[-c(1,2)])]
-	}
-	out
+  if (asGRanges) {
+    out <- GRanges(tmp$chrom, IRanges(tmp[, rcols[1]], tmp[, rcols[2]]))
+    elementMetadata(out) <- tmp[, names(cols)[4:14]] # drop first three columns
+  } else {
+    out <- tmp[, c("chrom", rcols, names(cols)[-c(1,2)])]
+  }
+  out
 }
