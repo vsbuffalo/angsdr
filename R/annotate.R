dd82af6056df56f165bc055f4451995dd6477d07
Vince Buffalo <vsbuffaloAAAAAA@gmail.com>
2014-03-06 17:53:37 -0800

    added a function to calc bases covered by a certain other set of ranges

---
+++ R/annotate.R
@@ -0,0 +1,46 @@
+## annotate.R -- annotate ANGSD GRanges objects with metadata
+
+#' Count overlapping bases between two \code{GRanges} objects
+#'
+#' \code{overlapWidths} returns a vector containing the number of bases each
+#' range in a \code{GRanges} object overlap with a another \code{GRanges}
+#' object (e.g. one of annotation exons). For example, if \code{x} is a
+#' \code{GRanges} object containing ANGSD windows, and \code{anno} is a
+#' \code{GRanges} object containing all exon ranges, \code{overlapWidths(x,
+#' anno)} will return a vector that can be used as \code{elementMetadata} in
+#' \code{x} to indicate how many bases in each range overlap \code{anno}.
+#'
+#' @param anno a \code{GenomicRanges} object contaning annotation ranges.
+#' @param angsd a \code{GenomicRanges} object containing windows and summary statistics from ANGSD.
+#' @param type the type of overlap to do; this passed directly to \code{\link{findOverlaps}}.
+#' @export
+#'
+#' @examples
+#' test_file <- system.file("inst", "extdata", "test.pestPG", package="angsdr")
+#' d <- readPestPG(test_file)
+#' anno <- GRanges("chr1", IRanges(c(70000, 120300), c(70300, 120800)))
+#' # create overlaps annotation
+#' overlapWidths(d, anno)
+#' # store in d
+#' d$anno_overlap <- overlapWidths(d, anno)
+overlapWidths <- function (x, anno,
+                           type=c("any", "start", "end", "within", "equal")) {
+  if (!is(x, "GRanges") || !is(anno, "GRanges"))
+    stop("both anno and x need to be GRanges objects")
+
+	# find overlaps between angsd windows and annotation
+	olaps <- findOverlaps(x, anno, type=type)
+
+	# extract overlappging ranges and the widths of these overlapping ranges
+	rr <- ranges(olaps, ranges(x), ranges(anno))
+	w <- width(rr)
+
+	# there are n widths, now, where n is the number of overlapping ranges (e.g.
+	# length(olaps)). We want to group these by where they fall in the ANGSD
+	# window, the findOverlaps query (e.g. length(olaps)). tapply() is the
+	# standard solution, but it is slow. As per suggested in http://bit.ly/NCnYPZ
+	# we use splitAsList.
+	o <- sum(splitAsList(w, factor(queryHits(olaps),
+																 seq_len(queryLength(olaps)))))
+	return(o)
+}
--- R/load.R
+++ R/load.R
@@ -39,30 +39,29 @@
 #'
 #' @export
 readPestPG <- function(file, asGRanges=TRUE, physical=FALSE) {
-
+	# we'll use colClasses in loading, for speed.
   cols <- c(region="character", chrom="factor", window.center="integer",
             theta_w="numeric", theta_pi="numeric", theta_fuli="numeric", theta_fayh="numeric",
             theta_l="numeric", taj_d="numeric", fuli_f="numeric", fuli_d="numeric",
             fay_h="numeric", zeng_e="numeric", numsites="integer")
 
   angsd_raw <- read.delim(file, header=FALSE, col.names=names(cols),
-													colClasses=cols, sep="\t")
+                          colClasses=cols, sep="\t")
 
 	# extract regions
   region_str <- gsub("\\((\\d+),(\\d+)\\)\\((\\d+),(\\d+)\\)\\((\\d+),(\\d+)\\)",
                      "\\1;;;\\2;;;\\3;;;\\4;;;\\5;;;\\6", angsd_raw$region, perl=TRUE)
 
 	regcols <- c("index_start", "index_stop", "pos_start", "pos_stop", "reg_start", "reg_stop")
-  region_chunks <- setNames(ldply(strsplit(region_str, ";;;"), .fun=as.numeric), regcols)
+  region_df <- setNames(ldply(strsplit(region_str, ";;;"), .fun=as.numeric), regcols)
 
 	# add parsed regions back in
-	tmp <- cbind(angsd_raw, region_chunks)
+	tmp <- cbind(angsd_raw, region_df)
 
   if (physical) {
-    # use the physical range used
-		rcols <- c("reg_start", "reg_stop")
-  } else {
 		rcols <- c("pos_start", "pos_stop")
+  } else {
+		rcols <- c("reg_start", "reg_stop")
   }
 
   if (asGRanges) {
@@ -73,3 +72,5 @@ readPestPG <- function(file, asGRanges=TRUE, physical=FALSE) {
   }
   out
 }
+
+
